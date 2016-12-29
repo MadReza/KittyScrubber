c6da3a711642e7d70f6eaab75e85ed6ffb2601f7
jratx <jrat123456@gmail.com>
2016-12-29 00:47:01 -0500

    adding page.php (temp name)

---
+++ page.php
@@ -0,0 +1,24 @@
+<?php include_once("header.php"); ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<button>Backwards</button>
+				<button>Forwards</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<img src="img/fairytail_cover.jpg">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<button>Report</button>
+				<button>Bookmark</button>
+				<button>Subscribe</button>
+			</div>
+		</div>
+	</div>
+
+<?php include_once("footer.php"); ?>
